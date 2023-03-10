ac8a30b34e8d27a3de66a6aef623b5746319e83a
吴俊 <8400684@qq.com>
2023-03-11 01:09:58 +0800

    add

---
+++ CoreMemoryPack/Packages/LoginPackage.cs
@@ -0,0 +1,25 @@
+﻿using MemoryPack;
+using TContentPackage;
+
+namespace CoreMemoryPack;
+
+[MemoryPackable]
+public sealed partial class LoginPackage : RpcPackageWithIdentifier
+{
+    public LoginPackage() : base(CommandKey.Login)
+    {
+    }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+}
+
+[MemoryPackable]
+public sealed partial class LoginRespPackage : RpcRespPackageWithIdentifier
+{
+    public LoginRespPackage() : base(CommandKey.LoginAck)
+    {
+    }
+}
+
