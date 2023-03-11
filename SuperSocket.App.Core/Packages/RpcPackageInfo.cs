c79a6ddeba1bee7ed7d73603a121d20e6f4052b2
吴俊 <8400684@qq.com>
2023-03-11 13:12:57 +0800

    添加

---
+++ SuperSocket.App.Core/Packages/RpcPackageInfo.cs
@@ -0,0 +1,96 @@
+﻿using Google.Protobuf;
+using SuperSocket.ProtoBase;
+
+namespace Core;
+
+public sealed partial class RpcPackageInfo : IKeyedPackageInfo<CommandKey>
+{
+    /// <summary>
+    /// 解析内容包
+    /// </summary>
+    /// <typeparam name="TContentPackage"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="KeyNotFoundException"></exception>
+    public TContentPackage DecodeBody<TContentPackage>() where TContentPackage : class, IMessage
+    {
+        var packageType = typeof(TContentPackage);
+
+        if (!CommandTypes.Value.TryGetValue(packageType, out var info))
+            throw new KeyNotFoundException(nameof(packageType));
+
+        return (TContentPackage)info.Parser(Content);
+    }
+
+    /// <summary>
+    /// 创建 rpc response
+    /// </summary>
+    /// <typeparam name="TContentPackage"></typeparam>
+    /// <returns></returns>
+    public RpcResponse<TContentPackage> CreateRpcResponse<TContentPackage>() where TContentPackage : class, IMessage
+    {
+        return new RpcResponse<TContentPackage>(this);
+    }
+
+    ///// <summary>
+    ///// 获取 command key
+    ///// </summary>
+    ///// <param name="packageType"></param>
+    ///// <returns></returns>
+    ///// <exception cref="KeyNotFoundException"></exception>
+    //public static CommandKey GetCommandKey(Type packageType)
+    //{
+    //    if (CommandTypes.Value.TryGetValue(packageType, out var info))
+    //        return info.Command;
+
+    //    throw new KeyNotFoundException(nameof(packageType));
+    //}
+
+    ///// <summary>
+    ///// 
+    ///// </summary>
+    ///// <typeparam name="TPackage"></typeparam>
+    ///// <returns></returns>
+    //public static CommandKey GetCommandKey<TPackage>()
+    //    where TPackage : class, IMessage
+    //{
+    //    return GetCommandKey(typeof(TPackage));
+    //}
+
+    //public static RpcPackageInfo Create<TContentPackage>(TContentPackage package) where TContentPackage : class, IMessage
+    //{
+    //    var body = package.ToByteArray();
+
+    //    var key = GetCommandKey<TContentPackage>();
+
+    //    return new RpcPackageInfo
+    //    {
+    //        Key = key,
+    //        Body = new ReadOnlySequence<byte>(body),
+    //    };
+    //}
+
+    //public static RpcPackageInfo Create<TContentPackage>(CommandKey key, TContentPackage package) where TContentPackage : class, IMessage
+    //{
+    //    var body = package.ToByteArray();
+
+    //    return new RpcPackageInfo
+    //    {
+    //        Key = key,
+    //        Body = new ReadOnlySequence<byte>(body),
+    //    };
+    //}
+
+    //public static RpcPackageInfo CreateForward<TContentPackage>(TContentPackage package) where TContentPackage : class, IMessage
+    //{
+    //    var body = package.ToByteArray();
+
+    //    var key = GetCommandKey<TContentPackage>();
+
+    //    return new RpcPackageInfo
+    //    {
+    //        ForwardKey = key,
+    //        Key = CommandKey.Forward,
+    //        Body = new ReadOnlySequence<byte>(body),
+    //    };
+    //}
+}
---
+++ SuperSocket.App.Core/Packages/RpcResponse.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf;
+
+namespace Core;
+
+public readonly struct RpcResponse<TContentPacket> where TContentPacket : class, IMessage
+{
+    public RpcResponse(RpcPackageInfo packet)
+    {
+        Successful = packet.SuccessFul;
+        ErrorCode = packet.ErrorCode;
+        ErrorMessage = packet.ErrorMessage;
+        Content = packet.DecodeBody<TContentPacket>();
+    }
+
+    /// <summary>
+    /// 包内容
+    /// </summary>
+    public readonly TContentPacket Content { get; }
+
+    /// <summary>
+    /// 是否成功
+    /// </summary>
+    public readonly bool Successful { get; }
+
+    /// <summary>
+    /// 错误代码
+    /// </summary>
+    public readonly ErrorCode ErrorCode { get; }
+
+    /// <summary>
+    /// 错误消息
+    /// </summary>
+    public readonly string? ErrorMessage { get; }
+}
